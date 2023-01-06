1caad2ed322e47afeb3503324fef56cdf3c04c3d
Bekzat Molutov <bekzat97plus@gmail.com>
2023-01-06 11:57:14 +0600

    needed apps are created and pgbd added, configured

--- onedev/onedev/settings.py
+++ onedev/onedev/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'onedev.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'onedev',
+        'USER': 'onedev',
+        'PASSWORD': 'pingpong',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
